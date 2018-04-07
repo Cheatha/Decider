ff2e245d83f6248099ed2f596e382773c9bdc4f4
Florian 'Cheatha' Köhler <git@cheatha.de>
2018-04-07 22:37:10 +0200

    check if db exists and if not, create it

--- start.sh
+++ start.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
+# Config
+db_name="names.db"
+db_path=$(dirname "$0")
+db="$db_path/$db_name"
 
-function open_DB() {
+
+function create_db() {
 # Open DB and if it doen't exist, create it
-
+if [ ! -f $db ]; then
+	 echo "Database not found!"
+	sqlite3 -batch $db "create table names (id INTEGER PRIMARY KEY,name TEXT,vote INTEGER,compared INTEGER);"
+else
+	sqlite3 -batch $db "select * from names"
+fi
 }
 
 function get_names() {
 # Gets n Options from the DB
-
+echo "Gets n Options from the DB"
 }
 
 function write_decision() {
 # Add deciscion to DB
+echo "Add deciscion to DB"
 
 }
 
@@ -26,5 +37,6 @@ function read_decision() {
 read -n 1 decision
 }
 
+create_db
 #present_options foo bar
 #read_decision
