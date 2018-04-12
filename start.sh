7ae7660842c3454e5ecf5c0cec784f3d13d97ad1
Florian 'Cheatha' Köhler <git@cheatha.de>
2018-04-12 10:28:43 +0200

    reorder functions

--- start.sh
+++ start.sh
@@ -17,7 +17,8 @@ default_name_count="3"
 default_vote_threshold="0" # Vote score an option needs to have to be picked 
 default_compared_threshold="10" # Number of rounds an option needs to have to been picked before vote threshold is counted
 
-# functions
+### Functions
+# program flow functions
 
 function check_input() {
 	case $mode in
@@ -51,148 +52,6 @@ function check_input() {
 			main_menu
 		;;
 		esac
-}
-
-function sql() {
-	local query="$2"
-	local mode="$1"
-	local options=""
-
-	case $mode in
-		list)
-		options="-line"
-		;;
-		query)
-		options=""
-		;;
-		column)
-		options="-column -header"
-		;;
-	esac
-
-	execute="sqlite3 ${options} ${db} \"${query}\""
-	eval $execute
-}
-
-function headline() {
-	local text="$1"
-	local text_length="$(echo $text|wc -m)"
-	echo -e "\n$text"
-	seq -s= $text_length|tr -d '[:digit:]'
-	echo ""
-}
-
-function create_db() {
-# Open DB and if it doen't exist, create it
-if [ ! -f $db ]; then
-	echo "Database not found!"
-	sql query "create table names (id INTEGER PRIMARY KEY,name TEXT UNIQUE,vote INTEGER,compared INTEGER);"
-fi
-}
-
-function add_name() {
-	name=$(sanitize $1)
-	echo "Add Name $name"
-
-	query=$(sql query 'select name from names where name='"'"'$name'"'"'')
-	if [[ -z "$query" ]]; then
-		sql query 'insert into names (name,vote,compared) values ('"'"'$name'"'"',0,0);'
-	else
-		echo "Skipping, $name already in DB"
-	fi
-}
-
-function remove_name() {
-	name=$(sanitize $1)
-	echo "Remove Name $name"
-	sql query 'delete from names where name='"'"'$name'"'"''
-}
-
-function hiscore() {
-	headline "Top 10 names"
-	sql column 'select vote, name from names order by vote desc limit 10'
-}
-
-function batch_add() {
-	# Add names from file
-	if [ ! -f $1 ]; then
-		echo "File $1 not found!"
-		exit 1
-	else
-		echo "File is $1"
-		while read name; do
-			add_name "$name"
-		done < "$1"
-	fi
-}
-
-function sanitize() {
-	lowercase=$(echo "$1"|tr '[:upper:]' '[:lower:]')
-	trimmed=$(echo "$lowercase"|tr -d '[:blank:]')
-	alpha=$(echo "$trimmed"|tr -cd  '[:alpha:]')
-	capitalize="$(tr '[:lower:]' '[:upper:]' <<< ${alpha:0:1})${alpha:1}"
-	echo "$capitalize"
-}
-
-function ask_options() {
-    vote_threshold="${global_vote_threshold:-${default_vote_threshold}}"
-    compared_threshold="${global_compared_threshold:-${default_compared_threshold}}"
-
-	# Gets n Options from the DB
-	headline "Select best option!"
-	names=$(sql query "select name from names where vote > $vote_threshold or compared < $compared_threshold order by random() limit $1")
-	options_left=$(echo $names|wc -w)
-
-	if [[ "$options_left" == "1" ]]; then
-		echo "Option $names has won!"
-		hiscore
-		exit 0
-	fi
-
-	local loop="1"
-	unset name_array
-	declare -a name_array
-	for name in $names;
-	do
-		i=$((loop++))
-		echo "[$i] $name"
-		name_array[$i]="$name"
-	done
-
-	echo -e "\n[q] Quit"
-
-	read_decision
-
-	case $decision in
-		q)
-		exit 0;
-		;;
-		[1-$i])
-		best=${name_array[$decision]}
-		write_decision "$best" "${name_array[*]}"
-		;;
-		*)
-		echo "Press Button 1-$i to choose the best option!"
-		sleep 5
-		;;
-	esac
-
-}
-
-function write_decision() {
-	# Add deciscion to DB
-	best="$1"
-	rest="$2"
-	for i in $rest; do
-		if [ "$i" == "$best" ]; then
-			sql query 'update names set vote = vote + 1 where name='"'"'$i'"'"''
-		else
-			sql query 'update names set vote = vote - 1 where name='"'"'$i'"'"''
-		fi
-
-		sql query 'update names set compared = compared + 1 where name='"'"'$i'"'"''
-
-	done
 }
 
 function main_menu() {
@@ -242,8 +101,164 @@ function main_menu() {
 	main_menu
 }
 
+function ask_options() {
+    vote_threshold="${global_vote_threshold:-${default_vote_threshold}}"
+    compared_threshold="${global_compared_threshold:-${default_compared_threshold}}"
+
+	# Gets n Options from the DB
+	headline "Select best option!"
+	names=$(sql query "select name from names where vote > $vote_threshold or compared < $compared_threshold order by random() limit $1")
+	options_left=$(echo $names|wc -w)
+
+	if [[ "$options_left" == "1" ]]; then
+		echo "Option $names has won!"
+		hiscore
+		exit 0
+	fi
+
+	local loop="1"
+	unset name_array
+	declare -a name_array
+	for name in $names;
+	do
+		i=$((loop++))
+		echo "[$i] $name"
+		name_array[$i]="$name"
+	done
+
+	echo -e "\n[q] Quit"
+
+	read_decision
+
+	case $decision in
+		q)
+		exit 0;
+		;;
+		[1-$i])
+		best=${name_array[$decision]}
+		write_decision "$best" "${name_array[*]}"
+		;;
+		*)
+		echo "Press Button 1-$i to choose the best option!"
+		sleep 5
+		;;
+	esac
+
+}
+
+
+# Database operation functions
+
+function create_db() {
+# Open DB and if it doen't exist, create it
+if [ ! -f $db ]; then
+	echo "Database not found!"
+	sql query "create table names (id INTEGER PRIMARY KEY,name TEXT UNIQUE,vote INTEGER,compared INTEGER);"
+fi
+}
+
+function sql() {
+	local query="$2"
+	local mode="$1"
+	local options=""
+
+	case $mode in
+		list)
+		options="-line"
+		;;
+		query)
+		options=""
+		;;
+		column)
+		options="-column -header"
+		;;
+	esac
+
+	execute="sqlite3 ${options} ${db} \"${query}\""
+	eval $execute
+}
+
+function write_decision() {
+	# Add deciscion to DB
+	best="$1"
+	rest="$2"
+	for i in $rest; do
+		if [ "$i" == "$best" ]; then
+			sql query 'update names set vote = vote + 1 where name='"'"'$i'"'"''
+		else
+			sql query 'update names set vote = vote - 1 where name='"'"'$i'"'"''
+		fi
+
+		sql query 'update names set compared = compared + 1 where name='"'"'$i'"'"''
+
+	done
+}
+
+# Helper functions
+
+function headline() {
+	local text="$1"
+	local text_length="$(echo $text|wc -m)"
+	echo -e "\n$text"
+	seq -s= $text_length|tr -d '[:digit:]'
+	echo ""
+}
+
+function sanitize() {
+	lowercase=$(echo "$1"|tr '[:upper:]' '[:lower:]')
+	trimmed=$(echo "$lowercase"|tr -d '[:blank:]')
+	alpha=$(echo "$trimmed"|tr -cd  '[:alpha:]')
+	capitalize="$(tr '[:lower:]' '[:upper:]' <<< ${alpha:0:1})${alpha:1}"
+	echo "$capitalize"
+}
+
 function read_decision() {
 	read -s -n 1 decision
+}
+
+# Program modes
+
+function decider() {
+	clear
+	name_count="${global_name_count:-${default_name_count}}"
+	ask_options $name_count
+	decider
+}
+
+function add_name() {
+	name=$(sanitize $1)
+	echo "Add Name $name"
+
+	query=$(sql query 'select name from names where name='"'"'$name'"'"'')
+	if [[ -z "$query" ]]; then
+		sql query 'insert into names (name,vote,compared) values ('"'"'$name'"'"',0,0);'
+	else
+		echo "Skipping, $name already in DB"
+	fi
+}
+
+function remove_name() {
+	name=$(sanitize $1)
+	echo "Remove Name $name"
+	sql query 'delete from names where name='"'"'$name'"'"''
+}
+
+function hiscore() {
+	headline "Top 10 names"
+	sql column 'select vote, name from names order by vote desc limit 10'
+}
+
+function batch_add() {
+	# Add names from file
+	if [ ! -f $1 ]; then
+		echo "File $1 not found!"
+		exit 1
+	else
+		echo "File is $1"
+		while read name; do
+			add_name "$name"
+		done < "$1"
+	fi
 }
 
 function print_db() {
@@ -252,13 +267,6 @@ function print_db() {
 	echo ""
 	entries=$(sql query "select count(name) from names")
 	echo "Number of names in database: $entries"
-}
-
-function decider() {
-	clear
-	name_count="${global_name_count:-${default_name_count}}"
-	ask_options $name_count
-	decider
 }
 
 function change_settings() {
